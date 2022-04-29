9799a408ceebe036d6d409d7c1c0385453aaaeae
victor flausino <78455613+victorflausino@users.noreply.github.com>
2022-04-29 18:05:09 -0300

    Update get_set1.php

--- get_set1.php
+++ get_set1.php
@@ -12,3 +12,45 @@ $valor3->setvalor3(10);
 
 echo '$valor1 * $valor2 * $valor3';
 ?>
+
+---------------------------------------------------------------
+<?php
+class valor {
+    
+    
+    private $valor1 = 0;
+    private $valor2 = 0;
+    private $valor3 = 0;
+
+    public function setValor1($valor1){
+        if (!is_numeric($valor1)){
+            echo "preço numeral.";
+            return;
+        }
+
+        $this->valor1 = $valor1;
+    }
+
+
+    public function setValor2($valor2){
+        if (!is_numeric($valor2)){
+            echo "preço numeral.";
+            return;
+        }
+
+        $this->valor2 = $valor2;
+    }
+
+
+    public function setValor3($valor3){
+        if (!is_numeric($valor3)){
+            echo "preço numeral.";
+            return;
+        }
+
+        $this->valor3 = $valor3;
+    }
+
+    
+    }
+?>
